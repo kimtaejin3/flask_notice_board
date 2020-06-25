31d0f391c93243625a2435a0851b5f9c6bace129
Taejin Kim <rlaxowls1316@naver.com>
2020-06-26 00:18:23 +0900

    first commit

---
+++ flask_notice_board-master/flask_board/run.py
@@ -0,0 +1,71 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_pymongo import PyMongo
+from datetime import datetime
+from flask import redirect
+from flask import url_for
+from bson.objectid import ObjectId
+from flask import abort
+import time
+
+app = Flask(__name__)
+app.config["MONGO_URI"]="mongodb://localhost:27017/myweb_prac"
+mongo = PyMongo(app)
+
+@app.template_filter("formatdatetime")
+def format_datetime(value):
+    if value is None:
+        return""
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp)
+    offset -= datetime.utcfromtimestamp(now_timestamp)
+    value = datetime.fromtimestamp((int(value) / 1000)) + offset
+    return value.strftime('%Y-%m-%d %H:%M:%S')
+
+
+@app.route("/view/<idx>")
+def board_view(idx):
+    # idx = request.args.get("idx")
+    if idx is not None:
+        board = mongo.db.board
+        data = board.find_one_and_update({"_id": ObjectId(idx)},{"$inc": {"view":1}}, return_document=True)
+
+        if data is not None:
+            result ={
+                "id": data.get("_id"),
+                "name": data.get("name"),
+                "title": data.get("title"),
+                "contents": data.get("contents"),
+                "pubdate": data.get("pubdate"),
+                "view": data.get("view"),
+            }
+        return render_template("view.html" ,result=result)
+    return abort(404)    
+
+
+@app.route("/write", methods=["GET", "POST"])
+def board_write():
+    if request.method=="POST":
+        name = request.form.get("name")
+        title = request.form.get("title")
+        contents = request.form.get("contents")
+
+        current_utc_time = round(datetime.utcnow().timestamp()*1000)
+        board = mongo.db.board
+        post = {
+            "name": name,
+            "title": title,
+            "contents": contents,
+            "pubdate": current_utc_time,
+            "view": 0,
+        }
+
+        x = board.insert_one(post)
+        return redirect(url_for('board_view', idx=x.inserted_id))
+    else:
+        return render_template('write.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
